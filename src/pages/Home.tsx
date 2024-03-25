93b74b8a126f81939621037e0d4d70cbb06e4a52
Yanick <yanick.burgermeister@gmail.com>
2024-03-25 20:49:10 +0100

    Startsiite no gmacht (Ich bin stolz uf mich)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,50 @@
 import "./Home.css"
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Paper from '@mui/material/Paper';
 
-function Home () {
+function Home() {
 
-        return (
-            <div>
-                <header>
-                    <h1>Willkommen im Zoo Zürich</h1>
-                </header>
-                <main>
-                    <section>
-                        <h2>Öffnungszeiten</h2>
-                        <p>Unser Zoo ist täglich von 9:00 bis 18:00 Uhr geöffnet.</p>
-                    </section>
-                    <section>
-                        <h2>Attraktionen</h2>
-                        <p>Besuchen Sie unsere vielfältige Tierwelt und erleben Sie spannende Attraktionen.</p>
-                    </section>
-                    <section>
-                        <h2>Anfahrt</h2>
-                        <p>Unser Zoo befindet sich in der Zürichbergstrasse 221, 8044 Zürich.</p>
-                    </section>
-                </main>
-                <footer>
-                    <p>&copy; 2024 Zoo Zürich. Alle Rechte vorbehalten.</p>
-                </footer>
-            </div>
-        );
+    return (
+        <div>
+            <Paper sx={{}} elevation={5} className={"paper"}>
+                <div id={"openingHoursTitle"}>
+                    <AccessTimeIcon fontSize={"large"}/>
+                    <h1>Öffnungszeiten</h1>
+                </div>
+                <table className={"openingHoursTable"}>
+                    <tr>
+                        <th>März bis Oktober</th>
+                    </tr>
+                    <tr>
+                        <td>Zoo: 9-18 Uhr</td>
+                    </tr>
+                    <tr>
+                        <td>Masoalahalle: 10-18 Uhr</td>
+                    </tr>
+                </table>
+                <br/>
+                <table className={"openingHoursTable"}>
+                    <tr>
+                        <th>November bis Februar</th>
+                    </tr>
+                    <tr>
+                        <td>Zoo: 9-17 Uhr</td>
+                    </tr>
+                    <tr>
+                        <td>Masoalahalle: 10-17 Uhr</td>
+                    </tr>
+                </table>
+            </Paper>
+            <Paper className={"newsPaper"}>
+                <a href={"https://www.zoo.ch/de/zoonews/zwei-neue-im-regenwald"}><img id={"news1Img"} src="../../public/Vogel.png" /></a>
+                <h3 className={"imageText"}>ZWEI NEUE IM REGENWALD</h3>
+            </Paper>
+            <Paper className={"newsPaper"}>
+                <a href={"https://www.zoo.ch/de/zoonews"}><img id={"news2Img"} src="../../public/KaTierHalt.png"/></a>
+                <h3 className={"imageText"}>GUT INFORMIERT: ZOONEWS</h3>
+            </Paper>
+        </div>
+    );
 }
 
 export default Home;
