494524b173108adc32dd70f658ee5fc324104a93
Yanick Burgermeister <118833108+YanickBurgermeister@users.noreply.github.com>
2024-03-26 11:23:45 +0100

    Feature/ZOO-004: Homepage (#7)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,50 @@
-function Home () {
+import "./Home.css"
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Paper from '@mui/material/Paper';
+
+function Home() {
+
     return (
-        <h1>Home</h1>
-    )
+        <div>
+            <Paper sx={{}} elevation={5} className={"paper"}>
+                <div id={"openingHoursTitle"}>
+                    <AccessTimeIcon fontSize={"large"}/>
+                    <h1>Öffnungszeiten</h1>
+                </div>
+                <table className={"openingHoursTable"}>
+                    <tr>
+                        <th>März bis Oktober</th>
+                    </tr>
+                    <tr>
+                        <td>Zoo: 9-18 Uhr</td>
+                    </tr>
+                    <tr>
+                        <td>Masoalahalle: 10-18 Uhr</td>
+                    </tr>
+                </table>
+                <br/>
+                <table className={"openingHoursTable"}>
+                    <tr>
+                        <th>November bis Februar</th>
+                    </tr>
+                    <tr>
+                        <td>Zoo: 9-17 Uhr</td>
+                    </tr>
+                    <tr>
+                        <td>Masoalahalle: 10-17 Uhr</td>
+                    </tr>
+                </table>
+            </Paper>
+            <Paper className={"newsPaper"}>
+                <a href={"https://www.zoo.ch/de/zoonews/zwei-neue-im-regenwald"}><img className={"newsImg"} src="public/Vogel.png" /></a>
+                <h3 className={"imageText"}>ZWEI NEUE IM REGENWALD</h3>
+            </Paper>
+            <Paper className={"newsPaper"}>
+                <a href={"https://www.zoo.ch/de/zoonews"}><img className={"newsImg"} src="public/KaTierHalt.png"/></a>
+                <h3 className={"imageText"}>GUT INFORMIERT: ZOONEWS</h3>
+            </Paper>
+        </div>
+    );
 }
 
 export default Home;
